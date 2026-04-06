f634cfa923abba42c7ff1a43acacfdb65dc8adae
H.W.S.M.MUNASINGHE <2022t01534@stu.cmb.ac.lk>
2026-04-07 00:07:23 +0530

    updated protected routes

--- ERP-frontend/src/routes/AppRouter.jsx
+++ ERP-frontend/src/routes/AppRouter.jsx
@@ -1,55 +1,75 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Toaster } from "sonner";
+import Sidebar from "../components/Sidebar";
 import Login from "../pages/Login";
-import Dashboard from "../pages/Dashboard";
 import Signup from "../pages/Signup";
 import ForgotPassword from "../pages/ForgotPassword";
 import ResetPassword from "../pages/ResetPassword";
 import ProtectedRoute from "./ProtectedRoute";
+import Dashboard from "../pages/Dashboard";
 import PurchaseOrderPage from "../pages/PurchaseOrderPage";
 import ProductsPage from "../pages/ProductsPage";
+import SupplierPage from "../pages/SupplierPage"; 
 
-function AppRouter() {
+function ProtectedPage({ children }) {
+  return (
+    <ProtectedRoute>
+      <div className="flex min-h-screen bg-gray-50">
+        <Sidebar />
+        <main className="flex-1 p-8">{children}</main>
+        <Toaster position="top-right" richColors />
+      </div>
+    </ProtectedRoute>
+  );
+}
+
+export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-
-        {/* Public routes */}
+        {/* Public */}
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password" element={<ResetPassword />} />
-
-        {/* Protected routes */}
+        
+        {/* Protected */}
         <Route
           path="/dashboard"
           element={
-            <ProtectedRoute>
+            <ProtectedPage>
               <Dashboard />
-            </ProtectedRoute>
+            </ProtectedPage>
           }
         />
-
         <Route
           path="/purchase-orders"
           element={
-            <ProtectedRoute>
+            <ProtectedPage>
               <PurchaseOrderPage />
-            </ProtectedRoute>
+            </ProtectedPage>
           }
         />
-
         <Route
           path="/products"
           element={
-            <ProtectedRoute>
+            <ProtectedPage>
               <ProductsPage />
-            </ProtectedRoute>
+            </ProtectedPage>
+          }
+        />
+        <Route
+          path="/supplier"
+          element={
+            <ProtectedPage>
+              <SupplierPage />
+            </ProtectedPage>
           }
         />
 
+        {/* Fallback */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
-export default AppRouter;
